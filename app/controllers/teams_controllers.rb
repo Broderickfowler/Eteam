fabfe51d234c8301817d61210d3615bd24b3741a
github username <github email address>
2020-07-22 18:44:55 -0700

    created teams/show

--- app/controllers/teams_controllers.rb
+++ app/controllers/teams_controllers.rb
@@ -8,9 +8,13 @@ class TeamsController < ApplicationController
         erb :'teams/index'
     end
 
-
     get '/teams/new' do
         erb :'teams/new'
+    end
+
+    get '/teams/:id' do
+        @team = current_user.teams.find(params[:id])
+        erb :'teams/show'
     end
 
     post '/teams' do
